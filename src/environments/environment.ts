ccf257f6f3184e4b567bb62ab3b176044090db4d
OskrVillVar <Alviva379000@gmail.com>
2026-08-20 01:29:14 -0600

    continuacion app - servicio de gifs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
 
-  production: true,
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
+production: true,
+companyName: "Gifs",
+companyName2: "App",
+companySlogan: "Maneja tus gifs",
 
-  //APIkeys
+giphyapiKey: '2YoZmSR0dm9TniWARYWis7csFxRmsF7I',
 
-  //URLS
-  
+
 };
