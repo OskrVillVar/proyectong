7c0abf7746d1014c91f46d592b194dc88d122dcd
OskrVillVar <Alviva379000@gmail.com>
2026-08-20 03:50:52 -0600

    mostrar gifs en pantalla

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,36 +1,27 @@
 import { Component, inject, signal } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
-import { GifListItem } from "../../components/gif-list/gif-list-item/gif-list-item";
-import { GifsService } from "../../services/gifs.service";
+import { GifService } from '../../services/gifs.service';
 
-const imageUrls: string[] = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-];
-
-
+// const imageUrls: string[] = [
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+// ];
 
 @Component({
   selector: 'trending-page.component',
   imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-
-export default class TrendingPage {
-
-gifs = signal(imageUrls);
-
-gifsService = inject( GifsService );
-
-
+export default class TrendingPageComponent {
+  gifService = inject(GifService)
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,36 +4,49 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class GifsService {
+export class GifService {
 
   private http = inject(HttpClient)
-  trendingGifs = signal<Gif[]>([]);
+
+  trendingGifs = signal<Gif[]>([])
+  trendingGifsloading = signal(true);
 
   constructor(){
-
-this.loadTrendingGifs();
-
-
+    this.loadTrendingGifs();
   }
 
   loadTrendingGifs(){
-    this.http.get<GiphyResponse>(`${ environment.giphyUrl}/gifs/trending`, {
+    this.http.get<GiphyResponse>( `${ environment.giphyUrl }/gifs/trending`, {
       params: {
         api_key: environment.giphyapiKey,
         limit: 20,
-
-      },
-    })
-    .subscribe(( resp ) => {
+      }
+    }).subscribe( (resp)  => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
       this.trendingGifs.set(gifs);
-
+      this.trendingGifsloading.set(false)
       console.log({ gifs });
+    });
+  }
 
-    })
+   searchGifs(query: string){
+    return this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/search`, {
+      params: {
+        api_key: environment.giphyapiKey,
+        limit: 20,
+        q: query,
+      }
+    }).pipe(
+      map(({data}) => data ),
+      map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+    );
 
-
+    // .subscribe( (resp)  => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+    //   console.log({ search: gifs });
+    // });
   }
 }
